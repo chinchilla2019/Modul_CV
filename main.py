529799c0ff618537a0371d2287f64535383b21c7
chinchilla2019 <chinchilla2019@yandex.ru>
2020-03-22 10:42:55 +1000

    ttttt

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import cv2
+import numpy as num
 
 # cv2.namedWindow("Image")
 # cv2.waitKey(0)
 
 image = cv2.imread("1.png")
 cv2.imshow("cat", image)
-cv2.waitKey(1000)
+cv2.waitKey(0)
 
 print(image.shape)
 (h, w) = image.shape[:2]
@@ -18,11 +19,11 @@ print(dim)
 
 resized = cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
 cv2.imshow("resized", resized)
-cv2.waitKey(1000)
+cv2.waitKey(0)
 
 cropped = image[10:150, 10:150]
 cv2.imshow("cropped", cropped)
-cv2.waitKey(1000)
+cv2.waitKey(0)
 
 (h, w) = image.shape[:2]
 center = (w // 2, h // 2)
@@ -30,17 +31,15 @@ center = (w // 2, h // 2)
 M = cv2.getRotationMatrix2D(center, 180, 1.0)
 rotated = cv2.warpAffine(image, M, (w, h))
 cv2.imshow("rotated", rotated)
-cv2.waitKey(1000)
+cv2.waitKey(0)
 
 rotated2 = cv2.flip(image, 1) # - from -1 to 1
 cv2.imshow("rotated2", rotated2)
-cv2.waitKey(1000)
+cv2.waitKey(0)
 
 
 color_spaces = ("RGB", "GRAY", "HSV", "LAB", "XYZ", "YUV")
-gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-cv2.imshow("gray_image", gray_image)
-cv2.waitKey(1000)
+
 
 color_image = {color: cv2.cvtColor(image, getattr(cv2, f'COLOR_BGR2{color}'))
                for color in color_spaces}
@@ -48,6 +47,21 @@ for clr in color_image:
     cv2.imshow(clr, color_image[clr])
 cv2.waitKey(0)
 
+l_red = (17, 50, 110)
+h_red = (101, 140, 180)
+only_cat = cv2.inRange(image, l_red, h_red)
+cv2.imshow("only_cat", only_cat)
+cv2.waitKey(0)
 
+clr = (255, 0, 0)
+clr = clr[::-1]
+mask = num.zeros((h + 2, w + 2), num.uint8)
+cv2.floodFill(image, mask, (h // 2, 80), clr)
+cv2.imshow("fioodfill", image)
+cv2.waitKey(0)
+
+cv2.putText(image, "CATTTTTT", (w // 2, h // 2), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 4)
+cv2.imshow("text", image)
+cv2.waitKey(0)
 
 cv2.destroyAllWindows()
